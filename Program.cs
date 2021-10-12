cb959f941cc0226b145bc7d70518910d07dfdb2f
thme04001 <91547411+thme04001@users.noreply.github.com>
2021-10-12 15:44:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace number_guesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Random nmr = new Random();
+            int tal = nmr.Next(1, 101);
+            Console.Write("Gissa ett heltal mellan 1 och 100: ");
+            int försök = 1;
+            while (true)
+            {
+                int svar = int.Parse(Console.ReadLine());
+            
+                if (svar < tal){
+                    Console.WriteLine("Du gissade för lågt, gissa igen!");
+                    försök++;
+                }
+                
+                else if (svar > tal){
+                    Console.WriteLine("Du gissade för högt, gissa igen!");
+                    försök++;
+                }
+
+                else if (svar == tal){
+                    Console.WriteLine($"Du gissade rätt efter {försök} försök!");
+                    break;
+                } 
+            }
+            
+        }
+    }
+}
